60f1d0f9a981797b7feab2711f746a234a72560f
caesar0016 <dapitoncaesar26@gmail.com>
2023-12-16 10:25:02 +0800

    Update:

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -47,9 +47,10 @@
             this.panelMain.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.panelMain.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panelMain.Location = new System.Drawing.Point(-1, 155);
+            this.panelMain.Location = new System.Drawing.Point(-1, 191);
+            this.panelMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(996, 435);
+            this.panelMain.Size = new System.Drawing.Size(1327, 534);
             this.panelMain.TabIndex = 2;
             // 
             // label1
@@ -60,9 +61,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(320, 16);
+            this.label1.Location = new System.Drawing.Point(427, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(356, 27);
+            this.label1.Size = new System.Drawing.Size(442, 35);
             this.label1.TabIndex = 1;
             this.label1.Text = "Diaz Rodriguez Boarding House";
             // 
@@ -71,9 +73,10 @@
             this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Location = new System.Drawing.Point(909, 4);
+            this.btnExit.Location = new System.Drawing.Point(1214, 5);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(100, 28);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "Close";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -92,8 +95,9 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(995, 154);
+            this.panel1.Size = new System.Drawing.Size(1327, 189);
             this.panel1.TabIndex = 0;
             // 
             // btnDashBoard
@@ -102,9 +106,10 @@
             this.btnDashBoard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnDashBoard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDashBoard.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDashBoard.Location = new System.Drawing.Point(75, 68);
+            this.btnDashBoard.Location = new System.Drawing.Point(102, 84);
+            this.btnDashBoard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDashBoard.Name = "btnDashBoard";
-            this.btnDashBoard.Size = new System.Drawing.Size(160, 48);
+            this.btnDashBoard.Size = new System.Drawing.Size(213, 59);
             this.btnDashBoard.TabIndex = 9;
             this.btnDashBoard.Text = "DashBoard";
             this.btnDashBoard.UseVisualStyleBackColor = false;
@@ -116,9 +121,10 @@
             this.btnTenant.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnTenant.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTenant.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTenant.Location = new System.Drawing.Point(263, 68);
+            this.btnTenant.Location = new System.Drawing.Point(353, 84);
+            this.btnTenant.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnTenant.Name = "btnTenant";
-            this.btnTenant.Size = new System.Drawing.Size(160, 48);
+            this.btnTenant.Size = new System.Drawing.Size(213, 59);
             this.btnTenant.TabIndex = 8;
             this.btnTenant.Text = "Tenant";
             this.btnTenant.UseVisualStyleBackColor = false;
@@ -130,9 +136,10 @@
             this.btnRoom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnRoom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRoom.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRoom.Location = new System.Drawing.Point(463, 68);
+            this.btnRoom.Location = new System.Drawing.Point(619, 84);
+            this.btnRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRoom.Name = "btnRoom";
-            this.btnRoom.Size = new System.Drawing.Size(160, 48);
+            this.btnRoom.Size = new System.Drawing.Size(213, 59);
             this.btnRoom.TabIndex = 7;
             this.btnRoom.Text = "Room";
             this.btnRoom.UseVisualStyleBackColor = false;
@@ -144,9 +151,10 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(670, 68);
+            this.button1.Location = new System.Drawing.Point(895, 84);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 48);
+            this.button1.Size = new System.Drawing.Size(213, 59);
             this.button1.TabIndex = 6;
             this.button1.Text = "Employee";
             this.button1.UseVisualStyleBackColor = false;
@@ -156,10 +164,10 @@
             // 
             this.profileMain.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.profileMain.Image = ((System.Drawing.Image)(resources.GetObject("profileMain.Image")));
-            this.profileMain.Location = new System.Drawing.Point(872, 41);
-            this.profileMain.Margin = new System.Windows.Forms.Padding(2);
+            this.profileMain.Location = new System.Drawing.Point(1163, 50);
+            this.profileMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.profileMain.Name = "profileMain";
-            this.profileMain.Size = new System.Drawing.Size(110, 98);
+            this.profileMain.Size = new System.Drawing.Size(147, 121);
             this.profileMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.profileMain.TabIndex = 0;
             this.profileMain.TabStop = false;
@@ -167,13 +175,14 @@
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(995, 588);
+            this.ClientSize = new System.Drawing.Size(1327, 724);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";

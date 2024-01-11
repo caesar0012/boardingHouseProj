f4ecadfdc4ece43d17ff37a7531f33ba1444f9d4
Aeron <Aeron Paul A. Amago@LAPTOP-PAMDIS9L>
2024-01-11 10:55:41 +0800

    Update: fixing connectiiong string for aeron's lappy

--- ConnectSql.cs
+++ ConnectSql.cs
@@ -11,15 +11,17 @@ namespace boardingHouseProj
     {
         //dapiton = @"Data Source=COLA\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True";
 
-        public static string connectionString = @"Data Source=DESKTOP-2GB3QJK\SQLEXPRESS01;Initial Catalog=BoardingHouse;Integrated Security=True;";
+        public static string connectionString = @"Data Source=LAPTOP-PAMDIS9L\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True";
         /*
-                static string edward = @"Data Source=DESKTOP-2GB3QJK\SQLEXPRESS01;Initial Catalog=BoardingHouse;Integrated Security=True";
+          edward = @"Data Source=DESKTOP-2GB3QJK\SQLEXPRESS01;Initial Catalog=BoardingHouse;Integrated Security=True";
 
-                static string Ronlenovo = @"Data Source=PC\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True";
+            Ronlenovo = @"Data Source=PC\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True";
 
-                static string dapiton = @"Data Source=COLA\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True";
+              apiton = @"Data Source=COLA\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True";
 
                 @"Data Source=LAPTOP-PAMDIS9L\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True;Trust Server Certificate=True"
+
+            aeron   @"Data Source=LAPTOP-PAMDIS9L\SQLEXPRESS;Initial Catalog=BoardingHouse;Integrated Security=True";
 
         */
 

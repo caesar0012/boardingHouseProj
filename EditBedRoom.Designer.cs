2f5a9d75a0012fd0f162a089281ed4debe4257cf
caesar0016 <dapitoncaesar26@gmail.com>
2024-01-29 18:29:19 +0800

    Update: Added form for bed editing status

---
+++ EditBedRoom.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace boardingHouseProj
+{
+    partial class EditBedRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgBed = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgBed
+            // 
+            this.dgBed.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBed.Location = new System.Drawing.Point(11, 46);
+            this.dgBed.Name = "dgBed";
+            this.dgBed.RowHeadersWidth = 51;
+            this.dgBed.RowTemplate.Height = 24;
+            this.dgBed.Size = new System.Drawing.Size(486, 532);
+            this.dgBed.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 602);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // EditBedRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(509, 637);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgBed);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "EditBedRoom";
+            this.Text = "EditBedRoom";
+            this.Load += new System.EventHandler(this.EditBedRoom_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgBed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgBed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ EditBedRoom.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace boardingHouseProj
+{
+    public partial class EditBedRoom : Form
+    {
+        public EditBedRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void EditBedRoom_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void loadData(string query) {
+
+            using (SqlConnection connect = new SqlConnection(ConnectSql.connectionString)) { 
+            
+                connect.Open();
+
+                SqlCommand cmd = new SqlCommand(query, connect);
+                cmd.ExecuteNonQuery();
+            
+            
+            }
+        
+        
+        
+        }
+
+    }
+}
